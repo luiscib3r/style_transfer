169c0f98e69a0f4fb7e2e844227e24ef1117cb2c
Luis Ciber <luisciber640@gmail.com>
2024-05-20 18:32:54 -0300

    chore: docker compose deploy

--- build_web_workers.sh
+++ build_web_workers.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 baseUrl=$1
+echo "Building web workers with base URL: $baseUrl"
 flutter_path=$(which flutter)
 flutter_web_sdk=$(dirname $flutter_path)/cache/flutter_web_sdk
+echo "Using Flutter Web SDK at: $flutter_web_sdk"
 dart compile js -O2 --libraries-spec=$flutter_web_sdk/libraries.json --define=baseUrl=$baseUrl -o web/styler_worker.dart.js lib/styler_worker.dart
 dart compile js -O2 --libraries-spec=$flutter_web_sdk/libraries.json --define=baseUrl=$baseUrl -o web/filter_worker.dart.js lib/filter_worker.dart
